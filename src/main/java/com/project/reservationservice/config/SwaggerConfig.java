e1b84a6ba977518f89bfda3cd213422edeae2c1c
LeeGeonu <rjsdndl909@gmail.com>
2024-07-07 21:00:47 +0900

    feat : swagger 수정

--- src/main/java/com/project/reservationservice/config/SwaggerConfig.java
+++ src/main/java/com/project/reservationservice/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.project.reservationservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +13,12 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Your API Title")
-                        .description("Your API Description")
+                        .title("테이블링 예약 API")
+                        .description("테이블링 예약 서비스를 위한 REST API 문서입니다.")
+                        .contact(new Contact()
+                        .name("이건우")
+                                        .email("rjsdndl909@gmail.com")
+                                        .url("https://github.com/geonuu09/Reservation-Service"))
                         .version("1.0"));
     }
 }
--- src/main/java/com/project/reservationservice/controller/ArrivalController.java
+++ src/main/java/com/project/reservationservice/controller/ArrivalController.java
@@ -1,6 +1,7 @@
 package com.project.reservationservice.controller;
 
 import com.project.reservationservice.service.ArrivalService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/arrival")
 @RequiredArgsConstructor
-@Tag(name = "Arrived Kiosk", description = "키오스크 도착확인 API")
+@Tag(name = "Kiosk", description = "키오스크 API")
 public class ArrivalController {
 
     private final ArrivalService arrivalService;
@@ -22,6 +23,10 @@ public class ArrivalController {
      * @param reservationId 예약 ID
      * @return 도착 확인 성공 여부에 대한 응답
      */
+    @Operation(
+            summary = "Arrived CheckIn",
+            description = "매장도착 체크인 (키오스크)"
+    )
     @PostMapping("/confirm")
     public ResponseEntity<String> confirmArrival(
             @RequestParam Long kioskId,
